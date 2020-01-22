c39746660c01c949dc9e7d4e168a99541b2aa447
giladd <giladd@trainologic.com>
2020-01-22 10:38:44 +0200

    Style a bit

--- lazy-load/src/components/App.jsx
+++ lazy-load/src/components/App.jsx
@@ -11,34 +11,35 @@ import Rare from "./Rare";
 // const Settings = React.lazy(() => import("./Settings"));
 // const Rare = React.lazy(() => import("./Rare"));
 
-
 const App = () => (
   <Router>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/topics">Topics</Link>
-      </li>
-      <li>
-        <Link to="/settings">Settings</Link>
-      </li>
-      <li>
-        <Link to="/rare">Rarely used route</Link>
-      </li>
-    </ul>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/topics">Topics</Link>
+        </li>
+        <li>
+          <Link to="/settings">Settings</Link>
+        </li>
+        <li>
+          <Link to="/rare">Rarely used route</Link>
+        </li>
+      </ul>
+    </nav>
 
-    <hr />
-
-    <React.Suspense fallback={<div>Loading...</div>}>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/topics" component={Topics} />
-        <Route path="/settings" component={Settings} />
-        <Route path="/rare" component={Rare} />
-      </Switch>
-    </React.Suspense>
+    <main>
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/topics" component={Topics} />
+          <Route path="/settings" component={Settings} />
+          <Route path="/rare" component={Rare} />
+        </Switch>
+      </React.Suspense>
+    </main>
   </Router>
 );
 
